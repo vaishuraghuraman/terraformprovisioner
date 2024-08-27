bddc5162d48e3f746326e7e02b9d0a27f2151e35
vaishuraghuraman <vaishuraghuraman20@gmail.com>
2024-08-27 13:37:02 +0530

    my first commit

---
+++ main.tf
@@ -0,0 +1,100 @@
+provider "google" {
+    project = var.project_id
+    region = var.region
+}
+
+
+//NETWORK
+
+resource "google_compute_network" "vpc_network" {
+ 
+  name                    =  var.networkname
+  auto_create_subnetworks = false
+  
+}
+
+//SUBNETWORK:
+resource "google_compute_subnetwork" "subnetwork" {
+  name          = var.subnetname
+  ip_cidr_range = var.ip_cidr_range
+  region        = var.region
+  network       = google_compute_network.vpc_network.name
+  secondary_ip_range {
+    range_name    = var.range_name
+    ip_cidr_range=  var.ip_cidr_range1
+  }
+}
+
+
+//FIREWALL
+resource "google_compute_firewall" "gh-9564-firewall-externalssh" {
+  name    = var.firewallname
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["externalssh"]
+}
+
+//VM 
+
+resource "google_compute_address" "static" {
+  name = "ipv4-address"
+}
+//VM
+
+resource "google_compute_instance" "vm1" {
+  name         = var.vmname
+  machine_type = var.machine_type
+  zone         = var.zone
+  tags = ["externalssh"]
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+ 
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    subnetwork = google_compute_subnetwork.subnetwork.name
+    access_config {
+      nat_ip = google_compute_address.static.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "vaishuraghuraman20@penguin:${file("/home/vaishuraghuraman20/key1.pub")}"
+  }
+provisioner "remote-exec" {
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      timeout     = "500s"
+      private_key = "${file("/home/vaishuraghuraman20/key1")}"
+      host = google_compute_instance.vm1.network_interface[0].access_config[0].nat_ip
+    }
+
+    inline = [
+      "touch /tmp/temp.txt",
+    ]
+  }
+
+
+  metadata_startup_script = "echo hi > /test.txt"
+
+
+
+  
+}
+
+
+
+
---
+++ var.tf
@@ -0,0 +1,51 @@
+variable "project_id" {
+    default = "qwiklabs-gcp-04-ab621185894d"
+}
+
+variable "region" {
+    default = "us-central1"
+  
+}
+
+variable "networkname"{
+    default = "mynetwork"
+}
+
+variable "firewallname"{
+    default = "myfirewall"
+}
+
+variable "subnetname" {
+   default = "mysubnet" 
+}
+
+
+variable "ip_cidr_range" {
+    default = "10.2.0.0/16"
+}
+
+variable range_name {
+    default ="tf-test-secondary-range-update1"
+}
+
+variable "ip_cidr_range1"{
+    default = "192.168.10.0/24"
+
+}
+
+variable "vmname"{
+    default = "myvm"
+}
+
+variable "machine_type" {
+    default = "n2-standard-2"
+}
+
+variable "zone" {
+    default = "us-central1-a"
+}
+
+variable "image" {
+    default = "debian-cloud/debian-11"
+}
+
